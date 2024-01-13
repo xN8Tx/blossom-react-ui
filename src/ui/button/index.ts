bcb6c94a75bcc22bdab96b24b075486bbc1327a8
Evgeniy Shteyn <theeldern8t@ya.ru>
2024-01-13 19:46:07 +0700

    fix: add loki and buttons imports

---
+++ src/ui/button/index.ts
@@ -0,0 +1,8 @@
+import { PrimaryButton } from './primary-button/PrimaryButton';
+import { SecondaryButton } from './secondary-button/SecondaryButton';
+
+import type { ButtonPropsType } from './index.types';
+import type { SecondaryButtonPropsType } from './secondary-button/SecondaryButton.types';
+
+export { PrimaryButton, SecondaryButton };
+export type { ButtonPropsType, SecondaryButtonPropsType };
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,5 +1,7 @@
+export { PrimaryButton, SecondaryButton } from './button';
 export { Paragraph } from './paragraph/Paragraph';
 export { Heading } from './heading/Heading';
 
+export type { ButtonPropsType, SecondaryButtonPropsType } from './button';
 export type { ParagraphPropsType } from './paragraph/Paragraph.types';
 export type { HeadingPropsType } from './heading/Heading.types';
